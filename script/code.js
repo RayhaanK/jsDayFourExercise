53efb64edf5626923609b877b4064fd0654ee3de
RayhaanK <rayhaankhan331@gmail.com>
2023-05-17 10:29:01 +0200

    added answers

---
+++ script/code.js
@@ -0,0 +1,19 @@
+// Question 1
+let person1 = {
+    firstName : "Jack",
+    age : 20
+};
+
+// Question Two
+let person2 = {
+    firstName : "Tyler",
+    age : 34
+};
+
+// Question Three
+console.log(person1);
+console.log(person2);
+
+// Question Four
+let people = [person1, person2]
+console.log(people);
